5e99464ce93f8c7ece7ce32742f7485d517e4cb5
Nadir Ahdid <nadir.ahdid@student.vinci.be>
2024-02-22 12:26:40 +0100

    fix: pipeline

--- src/main/java/be/vinci/pae/business/UserFactoryImpl.java
+++ src/main/java/be/vinci/pae/business/UserFactoryImpl.java
@@ -1,12 +1,21 @@
 package be.vinci.pae.business;
 
+/**
+ * Implementation of the {@code UserFactory} interface, providing methods to create instances
+ * of {@code UserDTO}.
+ */
 public class UserFactoryImpl implements UserFactory {
 
   /**
-   * @return
+   * Creates and returns a new instance of {@code UserDTO} representing a public user.
+   * This method is intended for creating user instances with limited information, suitable for
+   * public-facing contexts where sensitive details are not exposed.
+   *
+   * @return a new {@code UserDTO} instance representing a public user.
    */
   @Override
   public UserDTO getPublicUser() {
     return new UserImpl();
   }
 }
+
