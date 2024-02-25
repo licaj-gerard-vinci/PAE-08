4ad86a0d5463a78adacdbb49771ff2e1576cc2aa
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2024-02-25 15:36:22 +0100

    refactor: DAL services

--- src/main/java/be/vinci/pae/utils/ApplicationBinder.java
+++ src/main/java/be/vinci/pae/utils/ApplicationBinder.java
@@ -1,6 +1,16 @@
 package be.vinci.pae.utils;
 
-public class ApplicationBinder {
+import be.vinci.pae.dal.UserDAO;
+import be.vinci.pae.dal.UserDAOImpl;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.ext.Provider;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
+@Provider
+public class ApplicationBinder extends AbstractBinder {
 
+  @Override
+  protected void configure() {
+    bind(UserDAOImpl.class).to(UserDAO.class).in(Singleton.class);
+  }
 }
