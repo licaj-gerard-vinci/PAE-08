4a7e7ed419d94a6950b84330775d504c5bb6f624
Resul Ramadani <resul.ramadani@student.vinci.be>
2024-02-26 01:44:28 +0100

    add: code

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
+import { isAuthenticated } from '../../utils/auths';
 
 /**
  * Render the Navbar which is styled by using Bootstrap
@@ -9,35 +10,87 @@ import { Navbar as BootstrapNavbar } from 'bootstrap';
  */
 
 const Navbar = () => {
-  const navbarWrapper = document.querySelector('#navbarWrapper');
-  const navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-            <li class="nav-item">
-            <a class="nav-link" id="login" href="#" data-uri="/login">Login</a>
-          </li>
-                                      
-            </ul>
-          </div>
-        </div>
-      </nav>
-  `;
-
-  navbarWrapper.innerHTML = navbar;
+  renderNavbar();
 };
+
+
+  function renderNavbar() {
+
+    const unauthenticatedUser = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
+
+        <li class="nav-item">
+          <a class="nav-link" id="login" href="#" data-uri="/login">Se connecter</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#" data-uri="/register">Inscription</a>
+        </li> 
+                                  
+        </ul>
+      </div>
+    </div>
+  </nav>
+`;
+    
+    
+
+  const authenticatedUser = `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+      
+
+      <li class="nav-item">
+        <a class="nav-link" href="#" data-uri="/logout">Se deconnecter</a>
+      </li> 
+                                
+      </ul>
+    </div>
+  </div>
+</nav>
+`
+
+  const navbar = document.querySelector('#navbarWrapper');
+
+   
+
+  if (isAuthenticated()) {
+    navbar.innerHTML = authenticatedUser;
+  } 
+
+  else  {
+    navbar.innerHTML = unauthenticatedUser;
+  } 
+
+  }
+
 
 export default Navbar;
