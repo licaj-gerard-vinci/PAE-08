4050ef685fe19f6b9b2684544d60ea946ea27f62
Nadir Ahdid <nadir.ahdid@student.vinci.be>
2024-02-26 17:43:50 +0100

    fix: conflicts

--- src/main/java/be/vinci/pae/views/Views.java
+++ src/main/java/be/vinci/pae/views/Views.java
@@ -6,19 +6,19 @@ package be.vinci.pae.views;
  */
 public class Views {
 
-/**
- * The Public class represents a public JSON view.
- * Fields marked with this view will be included in public JSON responses.
- */
-public static class Public {
-}
+  /**
+   * The Public class represents a public JSON view.
+   * Fields marked with this view will be included in public JSON responses.
+   */
+  public static class Public {
+  }
 
-/**
- * The Internal class represents an internal JSON view.
- * Fields marked with this view will be included in internal JSON responses,
- * extending the Public view.
- */
-public static class Internal extends Public {
-}
+  /**
+   * The Internal class represents an internal JSON view.
+   * Fields marked with this view will be included in internal JSON responses,
+   * extending the Public view.
+   * */
+  public static class Internal extends Public {
+  }
 
-}
+  }
