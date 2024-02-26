e3cdec72d83d0066851589a7fc7a0feda918ed0f
Resul Ramadani <resul.ramadani@student.vinci.be>
2024-02-26 12:15:26 +0100

    add: fetch

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -1,13 +1,16 @@
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
 import Login from '../Pages/Login';
+import Logout from '../Logout/Logout';
 
 
 const routes = {
   '/': HomePage,
   '/new': NewPage,
   '/login': Login,
+  '/logout': Logout,
   
 };
 
 export default routes;
+
