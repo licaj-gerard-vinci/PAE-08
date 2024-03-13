7e39957ce247c95b8cc0da621b998dfee66dec77
Anthony De Vreese <anthony.devreese@student.vinci.be>
2024-03-13 12:15:28 +0100

    afix: pipeline v4.2

--- src/main/java/be/vinci/pae/dal/EntrepriseDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/EntrepriseDAOImpl.java
@@ -1,48 +1,48 @@
-    package be.vinci.pae.dal;
+package be.vinci.pae.dal;
 
-    import be.vinci.pae.business.EntrepriseDTO;
-    import be.vinci.pae.business.Factory;
-    import jakarta.inject.Inject;
+import be.vinci.pae.business.EntrepriseDTO;
+import be.vinci.pae.business.Factory;
+import jakarta.inject.Inject;
 
-    import java.sql.PreparedStatement;
-    import java.sql.ResultSet;
-    import java.sql.SQLException;
-    import java.util.ArrayList;
-    import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-    public class EntrepriseDAOImpl implements EntrepriseDAO {
-    @Inject
-    private DALService dalService;
-    @Inject
-    private Factory factory;
+public class EntrepriseDAOImpl implements EntrepriseDAO {
+@Inject
+private DALService dalService;
+@Inject
+private Factory factory;
 
-    /**
-     * Retrieves all entreprises from the database.
-     *
-     * @return a list of all entreprises.
-     */
-    @Override
-    public List<EntrepriseDTO> getEntreprises() {
-        String query = "SELECT ent.nom, ent.appellation," + " ent.adress, ent.numero_tel FROM pae.entreprises AS ent";
+/**
+ * Retrieves all entreprises from the database.
+ *
+ * @return a list of all entreprises.
+ */
+@Override
+public List<EntrepriseDTO> getEntreprises() {
+  String query = "SELECT ent.nom, ent.appellation," + " ent.adress, ent.numero_tel FROM pae.entreprises AS ent";
 
-        List<EntrepriseDTO> entreprises = new ArrayList<>();
+  List<EntrepriseDTO> entreprises = new ArrayList<>();
 
-        try (PreparedStatement statement = dalService.preparedStatement(query); ResultSet rs = statement.executeQuery()) {
-            while (rs.next()) {
-                entreprises.add(rsToEntreprises(rs));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return entreprises;
+  try (PreparedStatement statement = dalService.preparedStatement(query); ResultSet rs = statement.executeQuery()) {
+    while (rs.next()) {
+      entreprises.add(rsToEntreprises(rs));
     }
+  } catch (SQLException e) {
+    throw new RuntimeException(e);
+  }
+  return entreprises;
+}
 
-    private EntrepriseDTO rsToEntreprises(ResultSet rs) throws SQLException {
-        EntrepriseDTO entreprise = factory.getEntrepriseDTO();
-        entreprise.setNom(rs.getString("nom"));
-        entreprise.setAppellation(rs.getString("appellation"));
-        entreprise.setAdresse(rs.getString("adress"));
-        entreprise.setNumTel(rs.getString("numero_tel"));
-        return entreprise;
-    }
-    }
+private EntrepriseDTO rsToEntreprises(ResultSet rs) throws SQLException {
+  EntrepriseDTO entreprise = factory.getEntrepriseDTO();
+  entreprise.setNom(rs.getString("nom"));
+  entreprise.setAppellation(rs.getString("appellation"));
+  entreprise.setAdresse(rs.getString("adress"));
+  entreprise.setNumTel(rs.getString("numero_tel"));
+  return entreprise;
+}
+}
