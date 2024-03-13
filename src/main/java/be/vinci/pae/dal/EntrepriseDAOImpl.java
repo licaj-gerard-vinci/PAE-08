5fdc4179758ffe0020afedfd7d9678000357680d
Anthony De Vreese <anthony.devreese@student.vinci.be>
2024-03-13 12:19:52 +0100

    afix: pipeline v5

--- src/main/java/be/vinci/pae/dal/EntrepriseDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/EntrepriseDAOImpl.java
@@ -11,38 +11,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EntrepriseDAOImpl implements EntrepriseDAO {
-@Inject
-private DALService dalService;
-@Inject
-private Factory factory;
+  @Inject
+  private DALService dalService;
+  @Inject
+  private Factory factory;
 
-/**
- * Retrieves all entreprises from the database.
- *
- * @return a list of all entreprises.
- */
-@Override
-public List<EntrepriseDTO> getEntreprises() {
-  String query = "SELECT ent.nom, ent.appellation," + " ent.adress, ent.numero_tel FROM pae.entreprises AS ent";
+  /**
+  * Retrieves all entreprises from the database.
+  *
+  * @return a list of all entreprises.
+  */
+  @Override
+  public List<EntrepriseDTO> getEntreprises() {
+    String query = "SELECT ent.nom, ent.appellation," + " ent.adress, ent.numero_tel FROM pae.entreprises AS ent";
 
-  List<EntrepriseDTO> entreprises = new ArrayList<>();
+    List<EntrepriseDTO> entreprises = new ArrayList<>();
 
-  try (PreparedStatement statement = dalService.preparedStatement(query); ResultSet rs = statement.executeQuery()) {
-    while (rs.next()) {
-      entreprises.add(rsToEntreprises(rs));
+    try (PreparedStatement statement = dalService.preparedStatement(query); ResultSet rs = statement.executeQuery()) {
+      while (rs.next()) {
+        entreprises.add(rsToEntreprises(rs));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
-  } catch (SQLException e) {
-    throw new RuntimeException(e);
+    return entreprises;
   }
-  return entreprises;
-}
 
-private EntrepriseDTO rsToEntreprises(ResultSet rs) throws SQLException {
-  EntrepriseDTO entreprise = factory.getEntrepriseDTO();
-  entreprise.setNom(rs.getString("nom"));
-  entreprise.setAppellation(rs.getString("appellation"));
-  entreprise.setAdresse(rs.getString("adress"));
-  entreprise.setNumTel(rs.getString("numero_tel"));
-  return entreprise;
-}
+  private EntrepriseDTO rsToEntreprises(ResultSet rs) throws SQLException {
+    EntrepriseDTO entreprise = factory.getEntrepriseDTO();
+    entreprise.setNom(rs.getString("nom"));
+    entreprise.setAppellation(rs.getString("appellation"));
+    entreprise.setAdresse(rs.getString("adress"));
+    entreprise.setNumTel(rs.getString("numero_tel"));
+    return entreprise;
+  }
 }
