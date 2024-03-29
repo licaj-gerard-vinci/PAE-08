8b38acdb6dce8848136c5006ce2a9b346575aeb3
Gerard Licaj <gerard.licaj@student.vinci.be>
2024-03-30 00:27:55 +0100

    code: modified front list

--- webapp/src/Components/Pages/UserList.js
+++ webapp/src/Components/Pages/UserList.js
@@ -14,37 +14,40 @@ const UserList = () => {
   }
   if (authenticatedUser.user.role === 'E') {
     Navigate('/');
-    return;
   }
-  
 
-  renderUserList();
 }
+let currentPage = 1;
+const usersPerPage = 15;
+
 async function renderUserList() {
-    const main = document.querySelector('main');
+  const main = document.querySelector('main');
 
-
-    main.innerHTML = `
+  main.innerHTML = `
     <div class="container my-5">
       <h1 class="text-center mb-3">Recherche Utilisateurs</h1>
       <div class="row">
         <div class="col-12">
-          <div id="user-list-table-container" class="table-responsive"> 
+          <div id="user-list-table-container" class="table-responsive">
             <!-- Table will be inserted here -->
           </div>
         </div>
       </div>
     </div>`;
 
+  const userList = await getAllUsers();
 
-    const userList = await getAllUsers();
+  // Get current users
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = userList.slice(indexOfFirstUser, indexOfLastUser);
 
-    if (!userList || userList.length === 0) {
-        document.getElementById('user-list-table-container').innerHTML = `
+  if (!currentUsers || currentUsers.length === 0) {
+    document.getElementById('user-list-table-container').innerHTML = `
       <p class="text-center text-muted">Aucun utilisateur n'est disponible pour le moment ou vous n'avez pas les droits pour accéder à la page.</p>
     `;
-    } else {
-        const tableHtml = `
+  } else {
+    const tableHtml = `
         <div class="container">
         <div class="row">
             <div class="col-12">
@@ -59,10 +62,10 @@ async function renderUserList() {
                     <option value="E">Étudiant</option>
                     <option value="P">Professeur</option>
                     <option value="A">Administratif</option>
-                </select>              
-            </div>         
+                </select>
+            </div>
        </div>
-    </div> 
+    </div>
 </div>
       <table class="table table-hover shadow-sm">
         <thead class="table-dark">
@@ -75,7 +78,7 @@ async function renderUserList() {
           </tr>
         </thead>
         <tbody>
-          ${userList.map(user => `
+          ${currentUsers.map(user => `
             <tr class="${user.hasInternship ? 'table-success' : ''}">
               <td>${user.lastname}</td>
               <td>${user.firstname}</td>
@@ -86,16 +89,18 @@ async function renderUserList() {
           `).join('')}
         </tbody>
       </table>`;
-        document.getElementById('user-list-table-container').innerHTML = tableHtml;
-    }
+    document.getElementById('user-list-table-container').innerHTML = tableHtml;
+  }
 
-    // Filter by role
-    const filter = document.getElementById('filter');
-    filter.addEventListener('change', () => {
-        const selectedRole = filter.value;
-        const filteredUsers = userList.filter(user => selectedRole === 'all' ? true : user.role === selectedRole);
-        const tableBody = document.querySelector('tbody');
-        tableBody.innerHTML = filteredUsers.map(user => `
+  renderPagination(userList.length, usersPerPage);
+
+  // Filter by role
+  const filter = document.getElementById('filter');
+  filter.addEventListener('change', () => {
+    const selectedRole = filter.value;
+    const filteredUsers = userList.filter(user => selectedRole === 'all' ? true : user.role === selectedRole);
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = filteredUsers.map(user => `
           <tr class="${user.hasInternship ? 'table-success' : ''}">
             <td>${user.lastname}</td>
             <td>${user.firstname}</td>
@@ -104,24 +109,24 @@ async function renderUserList() {
             <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
             </tr>
         `).join('');
-    });
+  });
 
-    // Search by name
-    const searchBtn = document.getElementById('search-btn');
-    searchBtn.addEventListener('click', () => {
-        const search = document.getElementById('search').value;
-        const searchResults = userList.filter(user => user.lastname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()) || user.firstname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()));
-        if (searchResults.length === 0) {
-            const tableBody = document.querySelector('tbody');
-            tableBody.innerHTML = `
+  // Search by name
+  const searchBtn = document.getElementById('search-btn');
+  searchBtn.addEventListener('click', () => {
+    const search = document.getElementById('search').value;
+    const searchResults = userList.filter(user => user.lastname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()) || user.firstname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()));
+    if (searchResults.length === 0) {
+      const tableBody = document.querySelector('tbody');
+      tableBody.innerHTML = `
             <tr>
                 <td colspan="5" id="errorFiter" class="text-center" >Aucun résultat trouvé</td>
             </tr>
             `;
-            return;
-        }
-        const tableBody = document.querySelector('tbody');
-        tableBody.innerHTML = searchResults.map(user => `
+      return;
+    }
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = searchResults.map(user => `
           <tr class="${user.hasInternship ? 'table-success' : ''}">
             <td>${user.lastname}</td>
             <td>${user.firstname}</td>
@@ -131,6 +136,26 @@ async function renderUserList() {
             </tr>
         `).join('');
 
-    });
+  });
 }
+
+function paginate(pageNumber) {
+  currentPage = pageNumber;
+  renderUserList();
+}
+
+function renderPagination(totalUsers) {
+  const paginationContainer = document.createElement('div');
+
+  for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i+1) {
+    const pageLink = document.createElement('a');
+    pageLink.textContent = i;
+    pageLink.addEventListener('click', () => paginate(i));
+    paginationContainer.appendChild(pageLink);
+  }
+
+  document.body.appendChild(paginationContainer);
+}
+
+renderUserList();
 export default UserList;
