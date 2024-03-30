39a25040d4fbb737012ea51df5a9ba168e27b137
Gerard Licaj <gerard.licaj@student.vinci.be>
2024-03-30 03:18:59 +0100

    code: changed front

--- webapp/src/Components/Pages/UserList.js
+++ webapp/src/Components/Pages/UserList.js
@@ -23,7 +23,6 @@ const UserList = () => {
 async function renderUserList() {
   const main = document.querySelector('main');
 
-
   main.innerHTML = `
     <div class="container my-5">
       <h1 class="text-center mb-3">Recherche Utilisateurs</h1>
@@ -36,7 +35,6 @@ async function renderUserList() {
       </div>
     </div>`;
 
-
   const userList = await getAllUsers();
 
   if (!userList || userList.length === 0) {
@@ -46,20 +44,32 @@ async function renderUserList() {
   } else {
     const tableHtml = `
         <div class="container">
-        <div class="row">
+        <div class="row-filter">
             <div class="col-12">
                 <div class="form-group">
                 <label for="search">Rechercher :</label>
                 <input type="text" class="form-control" id="search" placeholder="Rechercher par nom ou prénom (nom.prenom / prenom.nom)">
-                <button class="btn btn-primary mt-3" id="search-btn">Rechercher</button>
+                <button class="btn btn-primary mt-2" id="search-btn">Rechercher</button>
                 </div>
+                
+                <div class="form-group2">
                 <label for="filter">Filtrer par rôle :</label>
                 <select class="form-control" id="filter">
                     <option value="all">Tous</option>
                     <option value="E">Étudiant</option>
                     <option value="P">Professeur</option>
                     <option value="A">Administratif</option>
-                </select>              
+                </select> 
+                </div>
+                <div class="form-group3">                 
+                 <label for="year">Filtrer par année académique :</label>
+                  <select class="form-control" id="year">
+                  <option value="all">Toutes</option>
+                  <option value="2022-2023">2022/2023</option>
+                  <option value="2023-2024">2023/2024</option>
+                  <option value="2024-2025">2024/2025</option>    
+            </select>
+            </div>
             </div>         
        </div>
     </div> 
@@ -79,8 +89,10 @@ async function renderUserList() {
             <tr class="${user.hasInternship ? 'table-success' : ''}">
               <td>${user.lastname}</td>
               <td>${user.firstname}</td>
-              <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
-              <td>${user.role !== 'E' ? 'N/A' : user.hasInternship ? 'Oui' : 'Non'}</td>
+              <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P'
+        ? 'Professeur' : 'Administratif'}</td>
+              <td>${user.role !== 'E' ? 'N/A' : user.hasInternship ? 'Oui'
+        : 'Non'}</td>
               <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
             </tr>
           `).join('')}
@@ -89,28 +101,15 @@ async function renderUserList() {
     document.getElementById('user-list-table-container').innerHTML = tableHtml;
   }
 
-  // Filter by role
-  const filter = document.getElementById('filter');
-  filter.addEventListener('change', () => {
-    const selectedRole = filter.value;
-    const filteredUsers = userList.filter(user => selectedRole === 'all' ? true : user.role === selectedRole);
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = filteredUsers.map(user => `
-          <tr class="${user.hasInternship ? 'table-success' : ''}">
-            <td>${user.lastname}</td>
-            <td>${user.firstname}</td>
-            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
-            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
-            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
-            </tr>
-        `).join('');
-  });
-
   // Search by name
   const searchBtn = document.getElementById('search-btn');
   searchBtn.addEventListener('click', () => {
     const search = document.getElementById('search').value;
-    const searchResults = userList.filter(user => user.lastname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()) || user.firstname.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search.toLowerCase()));
+    const searchResults = userList.filter(
+        user => user.lastname.toLowerCase().normalize("NFD").replace(
+                /[\u0300-\u036f]/g, "").includes(search.toLowerCase())
+            || user.firstname.toLowerCase().normalize("NFD").replace(
+                /[\u0300-\u036f]/g, "").includes(search.toLowerCase()));
     if (searchResults.length === 0) {
       const tableBody = document.querySelector('tbody');
       tableBody.innerHTML = `
@@ -125,6 +124,44 @@ async function renderUserList() {
           <tr class="${user.hasInternship ? 'table-success' : ''}">
             <td>${user.lastname}</td>
             <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P'
+        ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+            </tr>
+        `).join('');
+
+  });
+
+  // Filter by role
+  const filter = document.getElementById('filter');
+  filter.addEventListener('change', () => {
+    const selectedRole = filter.value;
+    const filteredUsers = userList.filter(
+        user => selectedRole === 'all' ? true : user.role === selectedRole);
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = filteredUsers.map(user => `
+          <tr class="${user.hasInternship ? 'table-success' : ''}">
+            <td>${user.lastname}</td>
+            <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+            </tr>
+        `).join('');
+  });
+
+  // Filter by year
+  const year = document.getElementById('year');
+  year.addEventListener('change', () => {
+    const selectedYear = year.value;
+    const filteredUsers = userList.filter(
+        user => selectedYear === 'all' ? true : user.year.annee === selectedYear);
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = filteredUsers.map(user => `
+        <tr class="${user.hasInternship ? 'table-success' : ''}">
+          <td>${user.lastname}</td>
+            <td>${user.firstname}</td>
             <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
             <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
             <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
@@ -133,4 +170,5 @@ async function renderUserList() {
 
   });
 }
+
 export default UserList;
