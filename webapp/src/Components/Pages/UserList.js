590190f8ea30e362ed4d3abe2a2da65a72cd3775
Anthony De Vreese <anthony.devreese@student.vinci.be>
2024-04-04 20:30:30 +0200

    add: changes in UserList, Router and Navigate

--- webapp/src/Components/Pages/UserList.js
+++ webapp/src/Components/Pages/UserList.js
@@ -3,28 +3,25 @@ import Navigate from '../Router/Navigate';
 import { getAuthenticatedUser } from '../../utils/auths';
 import { getAllUsers } from '../../model/users';
 
-
-
 const UserList = () => {
   const authenticatedUser = getAuthenticatedUser();
   if (!authenticatedUser) {
     Navigate('/');
     return;
-    
+
   }
   if (authenticatedUser.user.role === 'E') {
     Navigate('/');
     return;
   }
-  
+
 
   renderUserList();
 }
 async function renderUserList() {
-    const main = document.querySelector('main');
+  const main = document.querySelector('main');
 
-
-    main.innerHTML = `
+  main.innerHTML = `
     <div class="container my-5">
       <h1 class="text-center mb-3">Recherche Utilisateurs</h1>
       <div class="row">
@@ -36,16 +33,45 @@ async function renderUserList() {
       </div>
     </div>`;
 
+  const userList = await getAllUsers();
 
-    const userList = await getAllUsers();
-
-
-    if (!userList || userList.length === 0) {
-        document.getElementById('user-list-table-container').innerHTML = `
+  if (!userList || userList.length === 0) {
+    document.getElementById('user-list-table-container').innerHTML = `
       <p class="text-center text-muted">Aucun utilisateur n'est disponible pour le moment ou vous n'avez pas les droits pour accéder à la page.</p>
     `;
-    } else {
-        const tableHtml = `
+  } else {
+    const tableHtml = `
+        <div class="container">
+        <div class="row-filter">
+            <div class="col-12">
+                <div class="form-group">
+                <label for="search">Rechercher :</label>
+                <input type="text" class="form-control" id="search" placeholder="Rechercher par nom ou prénom (nom.prenom / prenom.nom)">
+                <button class="btn btn-primary mt-2" id="search-btn">Rechercher</button>
+                </div>
+                
+                <div class="form-group2">
+                <label for="filter">Filtrer par rôle :</label>
+                <select class="form-control" id="filter">
+                    <option value="all">Tous</option>
+                    <option value="E">Étudiant</option>
+                    <option value="P">Professeur</option>
+                    <option value="A">Administratif</option>
+                </select> 
+                </div>
+                <div class="form-group3">                 
+                 <label for="year">Filtrer par année académique :</label>
+                  <select class="form-control" id="year">
+                  <option value="all">Toutes</option>
+                  <option value="2022-2023">2022/2023</option>
+                  <option value="2023-2024">2023/2024</option>
+                  <option value="2024-2025">2024/2025</option>    
+            </select>
+            </div>
+            </div>         
+       </div>
+    </div> 
+</div>
       <table class="table table-hover shadow-sm">
         <thead class="table-dark">
           <tr>
@@ -56,21 +82,98 @@ async function renderUserList() {
             <th scope="col">Année Académique</th>
           </tr>
         </thead>
-        <tbody>
-          ${userList.map(user => `
-            <tr class="${user.hasInternship ? 'table-success' : ''}">
-              <td>${user.lastname}</td>
-              <td>${user.firstname}</td>
-              <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
-              <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
-              <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>`;
+      <tbody>
+        ${userList.map((user, index) => `
+          <tr id="dlas" class="${user.hasInternship ? 'table-success' : ''}">
+            <td class="profile-link" id="user-${index}">${user.lastname}</td>
+            <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.role !== 'E' ? 'N/A' : user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+          </tr>
+        `).join('')}
+      </tbody>
+    </table>`;
 
-        document.getElementById('user-list-table-container').innerHTML = tableHtml;
+
+    document.getElementById('user-list-table-container').innerHTML = tableHtml;
+
+    userList.forEach((user, index) => {
+      document.getElementById(`user-${index}`).addEventListener('click', () => {
+        if (user.role !== 'E') {
+            return;
+        }
+        Navigate('/studentInfo',user);
+      })});
+  };
+
+  // Search by name
+  const searchBtn = document.getElementById('search-btn');
+  searchBtn.addEventListener('click', () => {
+    const search = document.getElementById('search').value;
+    const searchResults = userList.filter(
+        user => user.lastname.toLowerCase().normalize("NFD").replace(
+                /[\u0300-\u036f]/g, "").includes(search.toLowerCase())
+            || user.firstname.toLowerCase().normalize("NFD").replace(
+                /[\u0300-\u036f]/g, "").includes(search.toLowerCase()));
+    if (searchResults.length === 0) {
+      const tableBody = document.querySelector('tbody');
+      tableBody.innerHTML = `
+            <tr>
+                <td colspan="5" id="errorFiter" class="text-center" >Aucun résultat trouvé</td>
+            </tr>
+            `;
+      return;
     }
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = searchResults.map(user => `
+          <tr class="${user.hasInternship ? 'table-success' : ''}">
+            <td>${user.lastname}</td>
+            <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P'
+        ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+            </tr>
+        `).join('');
+
+  });
+
+  // Filter by role
+  const filter = document.getElementById('filter');
+  filter.addEventListener('change', () => {
+    const selectedRole = filter.value;
+    const filteredUsers = userList.filter(
+        user => selectedRole === 'all' ? true : user.role === selectedRole);
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = filteredUsers.map(user => `
+          <tr class="${user.hasInternship ? 'table-success' : ''}">
+            <td>${user.lastname}</td>
+            <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+            </tr>
+        `).join('');
+  });
+
+  // Filter by year
+  const year = document.getElementById('year');
+  year.addEventListener('change', () => {
+    const selectedYear = year.value;
+    const filteredUsers = userList.filter(
+        user => selectedYear === 'all' ? true : user.year.annee === selectedYear);
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = filteredUsers.map(user => `
+        <tr class="${user.hasInternship ? 'table-success' : ''}">
+          <td>${user.lastname}</td>
+            <td>${user.firstname}</td>
+            <td>${user.role === 'E' ? 'Étudiant' : user.role === 'P' ? 'Professeur' : 'Administratif'}</td>
+            <td>${user.hasInternship ? 'Oui' : 'Non'}</td>
+            <td>${user.year.annee === null ? 'N/A' : user.year.annee}</td>
+            </tr>
+        `).join('');
+  });
 }
 
 export default UserList;
--- webapp/src/Components/Router/Router.js
+++ webapp/src/Components/Router/Router.js
@@ -27,11 +27,11 @@ function onNavBarClick() {
 }
 
 function onHistoryChange() {
-  window.addEventListener('popstate', () => {
+  window.addEventListener('popstate', (e) => {
     const uri = window.location.pathname;
     const componentToRender = routes[uri];
     if (!componentToRender) throw Error(`The ${uri} resource does not exist.`);
-    componentToRender();
+    componentToRender(e.state);
   });
 }
 
@@ -65,3 +65,5 @@ async function onFrontendLoad() {
 }
 
 export default Router;
+
+﻿
