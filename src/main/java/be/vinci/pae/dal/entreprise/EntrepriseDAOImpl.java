ee228420ee8b2736c67bebc4fa3fcc74359ac033
TiagoAmaralAndre <tiagolopes970@gmail.com>
2024-04-11 00:01:40 +0200

    fix: jewkins

--- src/main/java/be/vinci/pae/dal/entreprise/EntrepriseDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/entreprise/EntrepriseDAOImpl.java
@@ -118,7 +118,7 @@ public class EntrepriseDAOImpl implements EntrepriseDAO {
 
       try (ResultSet rs = statement.executeQuery()) {
         if (rs.next()) {
-        entreprise.setId(rs.getInt(1));
+          entreprise.setId(rs.getInt(1));
         }
       }
     } catch (SQLException e) {
@@ -127,11 +127,11 @@ public class EntrepriseDAOImpl implements EntrepriseDAO {
 
   }
 
-    /**
-     * Updates the entreprise in the database.
-     *
-     * @param entreprise The entreprise to update.
-     */
+  /**
+   * Updates the entreprise in the database.
+   *
+   * @param entreprise The entreprise to update.
+   */
   @Override
     public void updateEntreprise(EntrepriseDTO entreprise) {
     String query = "UPDATE pae.companies "
@@ -141,17 +141,17 @@ public class EntrepriseDAOImpl implements EntrepriseDAO {
                 + "company_version = company_version "
                 + "+ 1 WHERE company_id = ? AND company_version = ?";
     try (PreparedStatement statement = dalBackService.preparedStatement(query)) {
-        statement.setString(1, entreprise.getNom());
-        statement.setString(2, entreprise.getAdresse());
-        statement.setString(3, entreprise.getAppellation());
-        statement.setString(4, entreprise.getCity());
-        statement.setString(5, entreprise.getNumTel());
-        statement.setBoolean(6, entreprise.isBlackListed());
-        statement.setString(7, entreprise.getEmail());
-        statement.setString(8, entreprise.getMotivation_blacklist());
-        statement.setInt(9, entreprise.getId());
-        statement.setInt(10, entreprise.getVersion());
-        statement.executeUpdate();
+      statement.setString(1, entreprise.getNom());
+      statement.setString(2, entreprise.getAdresse());
+      statement.setString(3, entreprise.getAppellation());
+      statement.setString(4, entreprise.getCity());
+      statement.setString(5, entreprise.getNumTel());
+      statement.setBoolean(6, entreprise.isBlackListed());
+      statement.setString(7, entreprise.getEmail());
+      statement.setString(8, entreprise.getMotivation_blacklist());
+      statement.setInt(9, entreprise.getId());
+      statement.setInt(10, entreprise.getVersion());
+      statement.executeUpdate();
     } catch (SQLException e) {
         throw new FatalException(e);
     }
